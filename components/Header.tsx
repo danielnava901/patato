b8ea3ed7fc87951be56be13407fbaf431252f75f
daniel <danielnava901@hotmail.com>
2023-10-30 18:26:08 -0600

    commit

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,6 +3,7 @@ import {twMerge} from "tailwind-merge";
 import {useRouter} from "next/navigation";
 import {RxCaretLeft, RxCaretRight} from "react-icons/rx";
 import {HiHome} from "react-icons/hi";
+import Button from "./Button";
 
 interface HeaderProps {
     children: React.ReactNode,
@@ -99,7 +100,40 @@ export const Header: React.FC<HeaderProps> = (
                         " size={20}/>
                     </button>
                 </div>
+                <div className="
+                    flex
+                    justify-between
+                    items-center
+                    gap-x-4
+                ">
+                    <>
+                        <div>
+                            <Button className="
+                                bg-transparent
+                                text-neutral-300
+                                font-medium
+                            ">
+                                Sign up
+                            </Button>
+                        </div>
+                        <div>
+                            <Button
+                                onClick={() => {
+                                }}
+                                className="
+                                bg-white
+                                px-6
+                                py-2
+                            ">
+                                Log in
+                            </Button>
+                        </div>
+                    </>
+                </div>
             </div>
+            {
+                children
+            }
         </div>
     )
 }
