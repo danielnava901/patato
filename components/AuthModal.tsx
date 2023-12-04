4b7884d30973b30dd044ebb2e5696d14d2bd1c59
daniel <danielnava901@hotmail.com>
2023-12-04 17:24:31 -0600

    commit

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -1,15 +1,40 @@
 "use client"
 
 import Modal from "@/components/Modal";
+import {useSessionContext, useSupabaseClient} from "@supabase/auth-helpers-react";
+import {useRouter} from "next/navigation";
+import {Auth} from "@supabase/auth-ui-react";
+import {ThemeSupa} from "@supabase/auth-ui-shared";
 
 export const AuthModal = () => {
+    const supabaseClient = useSupabaseClient();
+    const router = useRouter();
+    const {session} = useSessionContext();
+
     return (
         <Modal isOpen
                onChange={() => {
                }}
                title="Bienvenido"
-               description="Ingresa a tu sesión">
-            Auth modal
+               description="Ingresa a tu sesión"
+        >
+            <Auth
+                theme="dark"
+                providers={["github"]}
+                magicLink
+                supabaseClient={supabaseClient}
+                appearance={{
+                    theme: ThemeSupa,
+                    variables: {
+                        default: {
+                            colors: {
+                                brand: "#404040",
+                                brandAccent: "22c55e"
+                            }
+                        }
+                    }
+                }}
+            />
         </Modal>
     )
 }
--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import {useEffect, useState} from "react";
-import Modal from "@/components/Modal";
 import {AuthModal} from "@/components/AuthModal";
 
 const ModalProvider = () => {
